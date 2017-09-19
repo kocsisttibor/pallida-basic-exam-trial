a7f1ceb7d61daed7f5cbbf70e91535c5169e482a
Kocsis <kocsisttibor@gmail.com>
2017-09-19 14:29:11 +0200

    feat(name_from_email): input and function created

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -5,4 +5,13 @@
 # example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
 # accents does not matter
 
-print(name_from_email("elek.viz@exam.com"))
+# print(name_from_email("elek.viz@exam.com"))
+
+user_email = input("Please enter an email-address: ")
+
+def name_from_email(email):
+    name = user_email.split("@")
+    first_last = name[0].split(".")
+    return first_last[1].capitalize() + " " + first_last[0].capitalize()
+
+print(name_from_email(user_email))
