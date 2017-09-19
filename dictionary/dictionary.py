ff9a497a1eac0204a0544694c2561b50e1abcce7
Kocsis <kocsisttibor@gmail.com>
2017-09-19 14:59:34 +0200

    feat(dictionary): translate_to_eng function created

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -12,12 +12,15 @@ def add_word(hun_word, eng_word):
     new_dict[hun_word] = eng_word
     dictionary.append(new_dict)
 
-add_word("citrom", "lemon")
-
-print(dictionary)
 
 # Implement these methods. They should return the translation of the given
 # word form the list 'dictionary'
+
+def keys():
+    keys = []
+    for i in range(len(dictionary)):
+        keys.append(list(dictionary[i].keys())[0])
+    return keys
 
 
 def translate_to_hun(eng_word):
@@ -25,4 +28,5 @@ def translate_to_hun(eng_word):
 
 
 def translate_to_eng(hun_word):
-    pass
+    return dictionary[keys().index(hun_word)][hun_word]
+
