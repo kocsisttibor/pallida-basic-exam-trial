7341a2cb24976ac29caa441ad2371493ca129894
Kocsis <kocsisttibor@gmail.com>
2017-09-19 15:59:34 +0200

    feat(odd_filter): TypeError exception handled

--- oddfilter/odd_filter.py
+++ oddfilter/odd_filter.py
@@ -5,12 +5,15 @@
 # print(odd_filter([1, 2, 3, 4, 5]))  # should print [1, 3, 5]
 
 def odd_filter(given_list):
-    odd_list = []
-    for i in given_list:
-        if i % 2 != 0:
-            odd_list.append(i)
-    return odd_list
+    try:
+        odd_list = []
+        for i in given_list:
+            if i % 2 != 0:
+                odd_list.append(i)
+        return odd_list
+    except TypeError:
+        print("The given parameter is not a list of numbers.")
+        return -1
 
-test_list = [1, 2, 3, 4, 5]
-
-print(odd_filter(test_list))
+alma = ["majd", 2, 3]
+odd_filter(alma)
